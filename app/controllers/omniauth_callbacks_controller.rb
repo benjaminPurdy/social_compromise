875d8bd50012d77d3507603d61b8b441d5a41238
benjaminPurdy <benjaminpurdy@hotmail.com>
2015-01-26 22:31:52 -0500

    got basic auth with facebook working, next up is getting it working with google, then some scss

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,17 +1,22 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def all
-    user = User.from_omniauth(request.env["omniauth.auth"]).to_yaml
+    user = User.from_omniauth(request.env['omniauth.auth'])
+    raise user.inspect
+
 
     if user.persisted?
-      flash.notice = "Signed in!"
+      flash.notice = 'Signed in!'
       sign_in_and_redirect user
     else
-      session["devise.user_attributes"] = user.attributes
+      session['devise.user_attributes'] = user.attributes
+      raise user.errors.inspect
       redirect_to new_user_registration_url
     end
   end
-  alias_method :twitter, :all
   alias_method :facebook, :all
+  alias_method :google_oauth2, :all
+
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :omniauthable,
@@ -8,15 +9,18 @@ class User < ActiveRecord::Base
   #validates_uniqueness_of :username
 
   def self.from_omniauth(auth)
-    where(auth.slice(:provider)).first_or_create do |user|
-      user.provider = auth.provider
-      user.username = auth.info.nickname
+    user_information = auth.slice(:provider, :uid, :info)
+    User.find_or_create_by(email: user_information.info.email) do |user|
+      user.provider = user_information.provider
+      user.uid = user_information.uid
+      user.email = user_information.info.email
     end
+
   end
 
   def self.new_with_session(params, session)
-    if session["devise.user_attributes"]
-      new(session["devise.user_attributes"], without_protection) do |user|
+    if session['devise.user_attributes']
+      new(session['devise.user_attributes'], without_protection) do |user|
         user.attributes = params
         user.valid?
       end
@@ -28,4 +32,5 @@ class User < ActiveRecord::Base
   def password_required?
     super && provider.blank?
   end
+
 end

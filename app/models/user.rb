6cc31bcb0eb153694856ebf269a1f85087c9041d
benjaminPurdy <benjaminpurdy@hotmail.com>
2015-01-26 20:05:15 -0500

    Pushing to new branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,31 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #validates_presence_of :username
+  #validates_uniqueness_of :username
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider)).first_or_create do |user|
+      user.provider = auth.provider
+      user.username = auth.info.nickname
+    end
+  end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection) do |user|
+        user.attributes = params
+        user.valid?
+      end
+     else
+       super
+     end
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
 end

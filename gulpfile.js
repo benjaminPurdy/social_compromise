d740585f4ab9018a6e155b4170f1e77b2ebba62a
benjaminPurdy <benjaminpurdy@hotmail.com>
2015-02-06 18:18:56 -0500

    adding some nodes

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,21 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    del = require('del');
+
+gulp.task('styles', function() {
+    return gulp.src('src/main/ui/stylesheets/*')
+        .pipe(sass())
+        .pipe(gulp.dest('src/main/rails/public')
+        .pipe(rename({suffix: '.min'})));
+});
+
+gulp.task('default', function() {
+    gulp.start('styles');
+});

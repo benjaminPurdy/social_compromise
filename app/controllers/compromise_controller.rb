e80e0d1a3e1e837fff2150c97b55f1db12c26056
Benjamin Purdy <benjaminpurdy@Benjamins-MacBook-Pro.local>
2015-07-01 10:50:53 -0400

    pushing current state

--- app/controllers/compromise_controller.rb
+++ app/controllers/compromise_controller.rb
@@ -6,8 +6,9 @@ class CompromiseController < ApplicationController
   def create
     compromise = Compromise.create!(title: params[:title], description: params[:description])
 
-    CompromiseUserMapping.create!(user_id: current_user.id, compromise_id: compromise.id)
-    params['members'].each do |key, value|
+    CompromiseUserMapping.create!(user_id: current_user.id, compromise_id: compromise.id, owner: true)
+    members = params[:members] || []
+    members.each do |key, value|
       user = User.find_by_display_name(key)
       CompromiseUserMapping.create!(user_id: user.id, compromise_id: compromise.id)
     end
@@ -16,7 +17,20 @@ class CompromiseController < ApplicationController
 
   def index
     @compromise = Compromise.find(params[:compromise])
-    @participant = CompromiseUserMapping.find_by(compromise_id: @compromise.id)
+    map = CompromiseUserMapping.where(compromise_id: @compromise.id)
+    @participants = []
+    @owner = nil;
+    map.each do |mapping|
+      user = User.find(mapping.user_id)
+      @participants.push user
+      if mapping.owner
+        @owner = user
+      end
+    end
+  end
+
+  def movie_vote
+
   end
 
   def edit

910f869e7bff272262a146f052d9d06693c38e59
benjaminpurdy <benjaminpurdy@hotmail.com>
2018-01-11 23:10:55 -0500

    removing irrelavent text and code out of a few more files to make it more clear what is needed and what isnt

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
+# removed require rails/all because i didnt want database dependencies
+
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
